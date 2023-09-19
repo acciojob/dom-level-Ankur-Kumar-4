f8bbef26943f6026e84e8675e37f5371972a8701
Ankur Kumar <98178905+Ankur-Kumar-4@users.noreply.github.com>
2023-09-19 18:23:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+const element = document.getElementById("level");
+
+function findLevel() {
+  let levelCount = 0;
+  let currentElement = element;
+  while (currentElement.parentNode) {
+    currentElement = currentElement.parentNode;
+    levelCount++;
+  }
+  return levelCount;
+}
+
+alert(findLevel());
